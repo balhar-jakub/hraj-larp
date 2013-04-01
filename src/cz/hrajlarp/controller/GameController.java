c0cefb721afd85a7c290a67f0749f2acf4d184ce
Jakub Balhar <balhar.jakub@gmail.com>
2013-04-01 11:08:07 +0200

    Merge posílání mailů při odesílání.

--- src/cz/hrajlarp/controller/GameController.java
+++ src/cz/hrajlarp/controller/GameController.java
@@ -1,13 +1,11 @@
 package cz.hrajlarp.controller;
 
 import cz.hrajlarp.model.*;
+import cz.hrajlarp.utils.FileUtils;
 import cz.hrajlarp.utils.MailService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -21,7 +19,6 @@ import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
-import java.security.Security;
 import java.util.List;
 
 
