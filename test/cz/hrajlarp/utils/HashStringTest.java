76c88449b57ab0ca8e3578cdf1d838f739003aaa
Jakub Balhar <balhar.jakub@gmail.com>
2013-04-02 16:23:05 +0200

    Kostra testů

---
+++ test/cz/hrajlarp/utils/HashStringTest.java
@@ -0,0 +1,16 @@
+package cz.hrajlarp.utils;
+
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jakub Balhar
+ * Date: 2.4.13
+ * Time: 16:15
+ */
+public class HashStringTest {
+    @Test
+    public void testDigest() throws Exception {
+
+    }
+}
