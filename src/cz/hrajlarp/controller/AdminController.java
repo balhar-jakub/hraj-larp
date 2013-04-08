33492dfd48377fdafa772e5777a91fbae27e358c
Jakub Balhar <balhar.jakub@gmail.com>
2013-04-08 17:39:40 +0200

    Administrátor chce odhlásit uživatele ze hry.

--- src/cz/hrajlarp/controller/AdminController.java
+++ src/cz/hrajlarp/controller/AdminController.java
@@ -1,6 +1,7 @@
 package cz.hrajlarp.controller;
 
 import cz.hrajlarp.model.*;
+import cz.hrajlarp.utils.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,13 +23,16 @@ import java.util.List;
 @Controller
 public class AdminController {
     @Autowired
-    GameDAO gameDAO;
+    private GameDAO gameDAO;
 
     @Autowired
-    UserDAO userDAO;
+    private UserDAO userDAO;
 
     @Autowired
-    UserAttendedGameDAO userAttendedGameDAO;
+    private UserAttendedGameDAO userAttendedGameDAO;
+
+    @Autowired
+    private MailService mailService;
 
     @RequestMapping(value = "/admin/game/players/{id}", method= RequestMethod.GET)
     public String gamePlayers(Model model, @PathVariable("id") Integer id) {
@@ -47,15 +51,48 @@ public class AdminController {
         }
     }
 
-    @RequestMapping(value = "/admin/game/logout/{gameId}/{playerId}", method= RequestMethod.GET)
+    @RequestMapping(value = "/admin/game/logout/{gameId}/{playerId}", method= RequestMethod.POST)
     public String logoutPlayer(Model model,
                                @PathVariable("gameId") Integer gameId,
                                @PathVariable("playerId") Integer playerId,
                                RedirectAttributes redirectAttributes) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        HrajUserEntity user = userDAO.getUserByLogin(auth.getName());
+        if (Rights.isLogged(auth) && user != null){
+            GameEntity game = gameDAO.getGameById(gameId);
+            HrajUserEntity oldUser = userDAO.getUserById(playerId);
+            UserAttendedGameEntity uage = new UserAttendedGameEntity();
+            uage.setGameId(gameId);
+            uage.setUserId(playerId);
+            if (userAttendedGameDAO.isLogged(uage)){   //user is logged in this game
+                userAttendedGameDAO.deleteUserAttendedGame(uage);   //logout old user
 
-        // TODO logout the user.
+                List<HrajUserEntity> assignedUsers = userAttendedGameDAO.getUsersByGameId(gameId);
+                game.setAssignedUsers(assignedUsers);   //count new free roles count
 
-        redirectAttributes.addFlashAttribute("gameId",gameId);
-        return "redirect:/admin/game/players";
+                int gender = 2;                                   //default setting for none men or women free roles, only both roles are free
+                if (oldUser.getGender()==0) {                     //loggouted user is man
+                    if (game.getMenFreeRoles() > 0) gender = 0;   //there are free men roles
+                }
+                else {                                            //loggouted user is woman
+                    if (game.getWomenFreeRoles() > 0) gender = 1; //there are free women roles
+                }
+
+                uage = userAttendedGameDAO.getFirstSubstitutedUAG(game.getId(), gender);  //get first substitute according to gender
+                if (uage != null){
+                    HrajUserEntity newUser = userDAO.getUserById(uage.getUserId());
+                    uage.setUserId(newUser.getId());
+                    uage.setSubstitute(false);
+                    userAttendedGameDAO.editUserAttendedGame(uage);             //edit this substitute as ordinary player
+                    mailService.sendMessage(newUser, game);
+                }
+            }
+
+            redirectAttributes.addAttribute("id",gameId);
+            return "redirect:/admin/game/players/{id}";
+        } else {
+            model.addAttribute("path", "/admin/game/logout/" + String.valueOf(gameId) + "/" + playerId);
+            return "/admin/norights";
+        }
     }
 }
