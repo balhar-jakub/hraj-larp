3d934a8ac22d661a12530bdc9cefae517944163f
Jakub Balhar <jakub@balhar.net>
2015-10-22 20:49:47 +0200

    Disable csrf

--- src/main/java/cz/hrajlarp/SecurityConfig.java
+++ src/main/java/cz/hrajlarp/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .csrf().disable()
             .authorizeRequests()
                 .antMatchers("/**")
                 .permitAll()
