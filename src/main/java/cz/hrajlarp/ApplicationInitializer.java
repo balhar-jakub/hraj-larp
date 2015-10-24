2217682afdadba0beade7b573b276136256cd262
Jakub Balhar <jakub@balhar.net>
2015-10-24 08:16:52 +0200

    Fix for Character encoding.

--- src/main/java/cz/hrajlarp/ApplicationInitializer.java
+++ src/main/java/cz/hrajlarp/ApplicationInitializer.java
@@ -1,11 +1,22 @@
 package cz.hrajlarp;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 /**
  * Web application initializer to remove XML from any configuration.
  */
 public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter cef = new CharacterEncodingFilter();
+        cef.setEncoding("UTF-8");
+        cef.setForceEncoding(true);
+        return new Filter[]{cef};
+    }
+
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
